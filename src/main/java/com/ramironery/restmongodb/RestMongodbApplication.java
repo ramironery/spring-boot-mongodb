6fc27ed38d03718f23e8d0487f555856c087753f
ramironery <ramironery@gmail.com>
2021-05-29 21:12:31 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/ramironery/restmongodb/RestMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ramironery.restmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMongodbApplication.class, args);
+	}
+
+}
