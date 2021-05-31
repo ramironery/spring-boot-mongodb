3f1ccc512591fa9ecc7a52cb8c334e1d79fa8945
ramironery <ramironery@gmail.com>
2021-05-31 10:46:05 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/ramironery/restmongodb/resources/exception/ResourceExceotionHandler.java
@@ -0,0 +1,22 @@
+package com.ramironery.restmongodb.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ramironery.restmongodb.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceotionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
