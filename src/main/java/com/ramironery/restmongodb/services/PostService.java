4b404e321b3805a3805c03307ea350761fcdf6c7
ramironery <ramironery@gmail.com>
2021-06-01 13:04:14 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ramironery/restmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.ramironery.restmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramironery.restmongodb.domain.Post;
+import com.ramironery.restmongodb.repository.PostRepository;
+import com.ramironery.restmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
