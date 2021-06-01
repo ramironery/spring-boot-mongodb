d24b722174d469688620a1b697c9eff243a69ee8
ramironery <ramironery@gmail.com>
2021-06-01 12:49:02 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/ramironery/restmongodb/repository/PostRepository.java
+++ src/main/java/com/ramironery/restmongodb/repository/PostRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ramironery.restmongodb.domain.Post;
-import com.ramironery.restmongodb.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
